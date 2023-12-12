59d91b794b2991df78abc7d5f9dfaaec6801ad3d
Lucas Andrade <academicolucas93@gmail.com>
2023-12-12 11:31:13 -0400

    Importado biblioteca render_template para uso de função que utiliza o template home.html como saída da rota.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+# Importado a funcionalidade 'render_template' para conectar rota ao arquivo home.html da pasta template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world(): 
-    return "<p>Hello, Oi!</p>"
+def hello_jovian(): 
+    # Saída da rota para o arquivo home.html usando o comando render_template
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
