4da3667d517c17b10eac9a46b3bceecaa4e77141
Lucas Andrade <academicolucas93@gmail.com>
2023-12-13 10:58:57 -0400

    Database structure created on app.py / DB variable called on home.html

--- app.py
+++ app.py
@@ -2,9 +2,39 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Estrutura de Banco de dados de empresas 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analist',
+        'location': 'Bengaluru, India',
+        'salary': 'R$ 10000'
+    },
+    {
+        'id': 2,
+        'title': 'Front-End Engineer',
+        'location': 'Delhi, India',
+        'salary': 'R$ 15000'
+    },
+    {
+        'id': 3,
+        'title': 'Data Scientist',
+        'location': 'Remote',
+        'salary': 'R$ 12000'
+    },
+    {
+        'id': 4,
+        'title': 'Back-End Engineer',
+        'location': 'San Francisco, USA',
+        'salary': '$ 12000'
+    }
+
+]
+
 @app.route("/")
 def hello_jovian(): 
-    return render_template('home.html')
+    # Passando a var do DB no retorno da rota
+    return render_template('home.html', jobs=JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
